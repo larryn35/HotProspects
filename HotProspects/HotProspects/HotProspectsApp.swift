482dc58ec254bff51375b11425251c5c3add3b21
Larry N <larryn35@gmail.com>
2020-09-26 14:32:46 -0700

    initial commit

---
+++ HotProspects/HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Larry Nguyen on 9/26/20.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
